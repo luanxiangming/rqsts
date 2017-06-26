898845a7188064b730e55a3aa56ec7a625021279
Oliver Luan <xiangming.luan@gmail.com>
2017-06-26 14:03:08 +0800

    generator expression

--- tutorial/iterators.py
+++ tutorial/iterators.py
@@ -16,7 +16,8 @@ class PythonIterators(unittest.TestCase):
 		pass
 
 	def test_iter_next(self):
-		it = iter(self.list)  # 创建迭代器对象
+		it = iter(self.list)  # 从可迭代对象创建迭代器对象
+		print(type(it))  # 迭代器有一种具体的迭代器类型: list_iterator
 		self.assertEqual(next(it), 1)
 		self.assertEqual(next(it), 2)
 
@@ -44,7 +45,7 @@ class PythonGenerators(unittest.TestCase):
 
 	@staticmethod
 	def fibonacci(n):  # 生成器函数 - 斐波那契
-		a, b, counter = 0, 1, 0
+		a, b, counter = 0, 1, 1
 		while True:
 			if counter > n:
 				return
@@ -60,6 +61,22 @@ class PythonGenerators(unittest.TestCase):
 			except StopIteration:
 				return
 
+	@staticmethod
+	def fibonacci2():
+		a, b = 0, 1
+		while True:
+			yield a
+			a, b = b, a + b
+
+	def test_generators(self):
+		self.assertEqual(list(itertools.islice(self.fibonacci2(), 0, 5)), [0, 1, 1, 2, 3])
+
+	def test_generator_expression(self):
+		""" 生成器表达式是列表推导的生成器版本， 它返回生成器对象而不是列表对象"""
+		gen = (x * x for x in itertools.count())
+		print(gen)
+		self.assertEqual(list(itertools.islice(gen, 5, 10)), [25, 36, 49, 64, 81])
+
 
 class PythonItertools(unittest.TestCase):
 	""" itertools模块提供的全部是处理迭代功能的函数，它们的返回值不是list，而是迭代对象，只有用for循环迭代的时候才真正计算
@@ -70,12 +87,12 @@ class PythonItertools(unittest.TestCase):
 		print(dir(itertools))
 
 		for c in itertools.count(5, 2):  # 从5开始的整数循环器，每次增加2，即5, 7, 9, 11, 13, 15 ...
-			print(c)
+			print(c, end=', ')
 			if c > 10:
 				break
 
 	def test_cycle(self):
-		cs = itertools.cycle('ABC')  # cycle()会把传入的一个序列无限重复下去
+		cs = itertools.cycle('ABC')  # cycle()会把传入的一个序列无限重复下去, 从有限序列中生成无限序列
 		times = 0
 		for c in cs:
 			times += 1
@@ -89,6 +106,7 @@ class PythonItertools(unittest.TestCase):
 
 	'''对于无限迭代, 可以通过takewhile()
 	等函数根据条件判断来截取出一个有限的序列'''
+
 	def test_takewhile(self):
 		naturals = itertools.count(1)
 		ns = itertools.takewhile(lambda x: x < 5, naturals)  # 当函数返回True时，收集元素到循环器。一旦函数返回False，则停止
@@ -99,40 +117,69 @@ class PythonItertools(unittest.TestCase):
 		self.assertEqual(list(it), [7, 1, 3])
 
 	''' 函数式工具: 这些函数接收函数作为参数，并将结果返回为一个循环器 '''
+
 	def test_starmap(self):
 		it = itertools.starmap(pow, [(1, 1), (2, 2), (3, 3)])  # pow将依次作用于表的每个tuple
 		self.assertEqual(list(it), [1, 4, 27])
 
 	''' chain()可以把一组迭代对象串联起来，形成一个更大的迭代器 '''
+
 	def test_chain(self):
 		it = itertools.chain('ABC', 'def')
 		self.assertEqual(list(it), ['A', 'B', 'C', 'd', 'e', 'f'])
 
 	''' 多个循环器集合的笛卡尔积。相当于嵌套循环 '''
+
 	def test_product(self):
 		it = itertools.product('ab', [1, 2])
 		self.assertEqual(list(it), [('a', 1), ('a', 2), ('b', 1), ('b', 2)])
 
 	''' 从'abc'中挑选两个元素，比如ab, bc, ... 将所有结果排序，返回为新的循环器 '''
+
 	def test_combinations(self):
 		it = itertools.combinations('abc', 2)
 		self.assertEqual(list(it), [('a', 'b'), ('a', 'c'), ('b', 'c')])
 
 	''' 把迭代器中相邻的重复元素挑出来放在一起 '''
+
 	def test_groupby(self):
 		s = sorted('cBAaAbccC', key=lambda a: a.upper())  # 分组之前需要使用sorted()对原循环器的元素，根据key函数进行排序，让同组元素先在位置上靠拢
 		for key, values in itertools.groupby(s, lambda a: a.upper()):  # 忽略大小写分组，就可以让元素'A'和'a'都返回相同的key
 			print(key, list(values))
 
 	''' 根据[1, 0, 1, 0, 0, 1]的真假值情况，选择第一个参数'ABCD'中的元素。P, t, n '''
+
 	def test_compress(self):
 		it = itertools.compress('Python', [1, 0, 1, 0, 0, 1])
 		self.assertEqual(list(it), ['P', 't', 'n'])
 
 	''' 类似于slice()函数，只是返回的是一个循环器 '''
+
 	def test_islice(self):
-		it = itertools.islice('Python', 3)
-		self.assertEqual(list(it), ['P', 'y', 't'])
+		infinite = itertools.cycle('Python')
+		finite = itertools.islice(infinite, 0, 7)  # 从无限序列中生成有限序列
+		self.assertEqual(list(finite), ['P', 'y', 't', 'h', 'o', 'n', 'P'])
+
+	class Fib:
+		def __init__(self):
+			self.prev = 0
+			self.curr = 1
+
+		def __iter__(self):  # 实现了__iter__表明Fib是可迭代对象
+			return self
+
+		def __next__(self):  # 实现了__next__表明Fib也是迭代器
+			value = self.curr
+
+			# 为下一次调用next()修改状态
+			self.curr += self.prev
+			self.prev = value
+
+			return value
+
+	def test_iter_fib(self):
+		fib = self.Fib()
+		print(list(itertools.islice(fib, 0, 10)))
 
 
 if __name__ == '__main__':
