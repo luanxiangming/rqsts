0bd3fa2fe64c71bf5702654de46e953d7a144847
Oliver Luan <xiangming.luan@gmail.com>
2017-09-21 01:24:13 +0800

    decorator and functools

--- tutorial/functions.py
+++ tutorial/functions.py
@@ -90,9 +90,61 @@ def test_cache():
 	print([fibonacci(n) for n in range(10)])
 
 
+def make_bold(func):
+	"""
+	装饰器要求入参是函数对象，返回值是函数对象，嵌套函数完全能胜任
+	"""
+	print("Initialize...")
+	cached_values = {}  # Contains already computed values
+
+	def wrapping_function(*args):
+		"""
+		因为返回的wrapper还在引用着，所以存在于make_bold命名空间的func不会消失。
+		make_bold可以装饰多个函数，wrapper不会调用混淆，因为每次调用make_bold，都会有创建新的命名空间和新的wrapper
+		"""
+		print("Call...")
+		if args not in cached_values:
+			# Call the function only if we've not already done it for those parameters
+			cached_values[args] = func(*args)
+		return cached_values[args]
+
+	return wrapping_function
+
+
+def make_italic(func):
+	"""
+	有时光伪装函数名和参数是不够的，因为Python的函数对象有一些元信息调用方可能读取了。为了连这些元信息也伪装上，functools.wraps出场了。
+	它能用于把被调用函数的__module__，__name__，__qualname__，__doc__，__annotations__赋值给装饰器返回的函数对象。
+	"""
+	@functools.wraps(func)
+	def wrapper(*args):
+		return '<h{0}>{1}</h{0}>'
+
+	return wrapper
+
+
+@make_italic
+# @make_bold
+def fibonacci2(n):
+	""" Return page content """
+	x, y = 0, 1
+	for i in range(n):
+		x, y = y, x + y
+	return x
+
+
+def test_nested():
+	print(fibonacci2.__name__)
+	print(fibonacci2.__doc__)
+
+	print([fibonacci2(n) for n in range(5)])
+
+print("functools: ")
+print(dir(functools))
 test_lambda()
 test_outer()
 test_global_var()
 test_mutable_args()
 test_cache()
+test_nested()
 test_returns()
